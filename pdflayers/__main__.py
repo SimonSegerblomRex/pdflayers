84fa57c3b53bfe7db44d84ee65ccc2ff396d2910
Simon Segerblom Rex <SimonSegerblomRex@users.noreply.github.com>
2019-05-20 11:31:50 +0200

    Restructure single script into package

--- pdflayers/__main__.py
+++ pdflayers/__main__.py
@@ -12,44 +12,12 @@ References:
 """
 import argparse
 import logging
-import sys
 
 import pikepdf
 
+from pdflayers import utils
 
-def set_layer_visibility(pdf, layers_to_show):
-    """Set visibility of layers."""
-    try:
-        ocgs = pdf.root.OCProperties.OCGs
-    except (AttributeError, KeyError):
-        logging.error("Unable to locate layers in PDF.")
-        sys.exit(1)
-
-    ocgs_on = []
-    for ocg in ocgs:
-        if ocg.Name in layers_to_show:
-            logging.info("Layer %s will be visible.", ocg.Name)
-            ocgs_on.append(ocg)
-        else:
-            logging.info("Layer %s will be hidden.", ocg.Name)
-
-    ocgs_config = pikepdf.Dictionary(
-        BaseState=pikepdf.Name('/OFF'),
-        ON=ocgs_on,
-        Order=ocgs,
-    )
-
-    pdf.root.OCProperties = pikepdf.Dictionary(
-        D=ocgs_config,
-        OCGs=ocgs,
-    )
-
-    # Needed for google-chrome (at least):
-    for ocg in ocgs:
-        if '/View' in ocg.Usage:
-            del ocg.Usage.View
-        if '/Print' in ocg.Usage:
-            del ocg.Usage.Print
+logger = logging.getLogger(__name__)
 
 
 def main():
@@ -72,7 +40,7 @@ def main():
 
     pdf = pikepdf.open(args.input, password=args.password)
 
-    set_layer_visibility(pdf, args.show)
+    utils.set_layer_visibility(pdf, args.show)
     pdf.remove_unreferenced_resources()
 
     save_options = {
